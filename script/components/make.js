2b68a11529ba8b546adcf204538af6c44542ed11
Olga Albukova <o.albukova@gmail.com>
2020-08-16 11:16:10 +0300

    fix block

--- script/components/make.js
+++ script/components/make.js
@@ -1,4 +1,6 @@
-import { api } from "../components/Api.js";
+import {
+  api
+} from "../components/Api.js";
 import {
   ingridientInput,
   amountInput,
@@ -13,36 +15,53 @@ api
   .then((res) => {
     console.log(res);
     //создаем в контейнере с id result список ul с перечислением ингридиентов
-    let ingridientsList = "<ul>";
+    let ingridientsList = "";
     for (let i = 0; i < res.length; i++) {
       ingridientsList +=
-        "<li class='input_ingridients-list'>" + res[i].title + "</li>";
+        "<li>" + res[i].title + "</li>";
     }
+
     document.getElementById("ingridient-result").innerHTML = ingridientsList;
+
     //для каждого элемента - если он не содержит значения инпута, то он скрывается классом hide
     //запускается при любом изменении инпута - так что если буква появилась - он появляется
+    let array = Object.values(document.querySelectorAll(".select li"));
+    console.log(array);
+
     ingridientInput.oninput = function () {
       let val = this.value.trim(); //значение инпута без пробелов
+
       if (val != "") {
-        res.forEach((elem) => {
-          if (elem.innerText.search(val) == -1) {
+        console.log(val);
+        array.forEach((elem) => {
+          if (elem.innerHTML.toLowerCase().search(val) == -1) {
             elem.classList.add("hide");
           } else {
             elem.classList.remove("hide");
-            //меняет весовые значения
-            ingridientSpan.textContent = res(
-              elem.innerText.search(val)
-            ).dimension;
+            // меняет весовые значения НЕ ОТТЕСТИРОВАЛА, ТАК КАК ЛОГИКА ЗАВЯЗАНА НА 
+            //МАССИВ, А НЕ РЕС
+            // ingridientSpan.textContent = res(
+            //   elem.innerText.search(val)
+            // ).dimension;
           }
         });
       } else {
-        res.forEach((elem) => {
+        array.forEach((elem) => {
           elem.classList.remove("hide");
         });
-      }
+      };
       //это чтоб запомнить вводимое значение, тут я сомневаюсь
       ingridientInput.value = val;
     };
+
+    ingridientInput.addEventListener('focus', () => {
+      document.getElementById("ingridient-result").classList.toggle('select_hidden');
+    })
+
+    ingridientInput.addEventListener('blur', () => {
+      document.getElementById("ingridient-result").classList.toggle('select_hidden');
+    })
+
     amountInput.onblur = function () {
       amountInput.value = this.value;
     };
