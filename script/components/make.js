64098be1745d915cd6cd144bb778383ff7c653f7
Metanka <themetanka@gmail.com>
2020-08-17 10:55:32 +0300

    по клику можно выбрать ингридиент

--- script/components/make.js
+++ script/components/make.js
@@ -1,4 +1,4 @@
-import { api } from "../components/Api.js";
+import {api} from "../components/Api.js";
 import {
     ingridientInput,
     amountInput,
@@ -6,10 +6,10 @@ import {
     addIngridients,
 } from "../constants.js";
 //import {stars} from "../constants";
-const query = ingridientInput.textContent;
+let query = ingridientInput.textContent;
 const add = (evt) => { //при нажатии на кнопку меняет классы и блокирует ее
-    console.log('5');
-    evt.target.textContent = query;
+    ingridientInput.value = evt.target.textContent
+    document.getElementById("ingridient-result").classList.toggle('select_hidden');
 }
 api
     .search(query)
@@ -21,34 +21,25 @@ api
             ingridientsList +=
                 "<li class='item'>" + res[i].title + "</li>";
             const item = document.querySelector('.item');
-           // item.setAttribute('data-id', '2');
+            // item.setAttribute('data-id', '2');
         }
         document.getElementById("ingridient-result").innerHTML = ingridientsList;
         //для каждого элемента - если он не содержит значения инпута, то он скрывается классом hide
         //запускается при любом изменении инпута - так что если буква появилась - он появляется
         let array = Object.values(document.querySelectorAll(".select li"));
-        console.log(array);
         array.forEach(elem => {
-            console.log('1')
-            elem.addEventListener('click', add);
+            elem.addEventListener('mousedown', add);
         })
         ingridientInput.oninput = function () {
             let val = this.value.trim(); //значение инпута без пробелов
             if (val !== "") {
-                console.log(val);
                 array.forEach((elem) => {
                     if (elem.innerHTML.toLowerCase().search(val) === -1) {
                         elem.classList.add("hide");
                     } else {
                         elem.classList.remove("hide");
-                        console.log(elem.textContent);
                         //добавить класс этим элементам, искать по классу и навесить слушатели.
-                        elem.addEventListener('click', add)
-                        // меняет весовые значения НЕ ОТТЕСТИРОВАЛА, ТАК КАК ЛОГИКА ЗАВЯЗАНА НА
-                        //МАССИВ, А НЕ РЕС
-                        // ingridientSpan.textContent = res(
-                        //   elem.innerText.search(val)
-                        // ).dimension;
+                        elem.addEventListener('click', add);
                     }
                 });
             } else {
@@ -56,15 +47,16 @@ api
                     elem.classList.remove("hide");
                 });
             };
-            //это чтоб запомнить вводимое значение, тут я сомневаюсь
-            ingridientInput.value = val;
         };
+
+        document.addEventListener('blur', () => {
+            document.getElementById("ingridient-result").classList.add('select_hidden');
+        })
         ingridientInput.addEventListener('focus', () => {
-            document.getElementById("ingridient-result").classList.toggle('select_hidden');
+            document.getElementById("ingridient-result").classList.remove('select_hidden');
         })
-        ingridientInput.addEventListener('blur', () => {
-            document.getElementById("ingridient-result").classList.toggle('select_hidden');
-        })
+
+
         amountInput.onblur = function () {
             amountInput.value = this.value;
         };
